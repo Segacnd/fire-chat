042a8be6971eb8cb570e5b0b186783d0b6eba61c
Segacnd <segabelka@yandex.ru>
2023-04-22 13:58:52 +0300

    add linting, readme, feature-sliced and config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,84 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  overrides: [
+    {
+      files: ['config/**/*.ts', 'src/global-definitions.d.ts', 'src/libs.d.ts'],
+      rules: {
+        'import/no-default-export': 'off',
+      },
+    },
+    {
+      files: ['src/redux/slices/*.ts'],
+      rules: {
+        'no-param-reassign': 'off',
+        'jsx-a11y/label-has-associated-control': 'off',
+      },
+    },
+    {
+      files: ['**/*.tsx'],
+      rules: {
+        'react/require-default-props': 'off',
+      },
+    },
+    {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      extends: ['plugin:@typescript-eslint/recommended'],
+      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+    },
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id'],
+        },
+      },
+    ],
+    'jsx-a11y/label-has-for': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id'],
+        },
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/*.test.{ts,tsx,js,jsx}'],
+      },
+    ],
+    'import/no-default-export': 'error',
+    'import/prefer-default-export': 'off',
+    'react/react-in-jsx-scope': 0,
+    indent: 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^action' }],
+    'no-nested-ternary': 'off',
+    'no-unneeded-ternary': 'off',
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
   },
-}
+};
---
+++ firebase.config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBs4PV7XlMxqYnp0Gc7JjrqBI_NfYl8WkY",
+  authDomain: "fire-chat-2b80f.firebaseapp.com",
+  projectId: "fire-chat-2b80f",
+  storageBucket: "fire-chat-2b80f.appspot.com",
+  messagingSenderId: "899676576885",
+  appId: "1:899676576885:web:8efd8f4fedc1f3da1fac3c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
